3f43e63be0adc6d48efb8e62433be2295a9bcc9c
Vitaly <65219683+Vitaly3000@users.noreply.github.com>
2020-07-24 19:37:22 +0300

    first

---
+++ app/controllers/MainController.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace app\controllers;
+
+
+class MainController extends AppController
+{
+
+    public function indexAction()
+    {
+        echo __METHOD__;
+        
+
+    }
+
+}
--- public/index.php
+++ public/index.php
@@ -1,8 +1,5 @@
 <?php
 require_once dirname(__DIR__) . "/config/init.php";
 require_once LIBS . '/functions.php';
+require_once CONF . '/routes.php';
 new \shop\App();
-
-\shop\App::$app->setProperty('test', 'TEST');
-
-throw new Exception('Страница не найдена', '404');
